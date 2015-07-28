97dc38b5b56f03994a3853fad57b9ed6a454206e
Peter Connolly <pmconnolly80@gmail.com>
2015-07-28 15:28:06 -0500

    JSON into right continer

--- music-history-main.js
+++ music-history-main.js
@@ -1,32 +1,27 @@
-var songs = [];
-var songList = document.getElementById("song-list");
+$(document).ready(function() {
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+var songString = "";
+
+	$.ajax({
+		url: "list.JSON"
+	}).done(function(data) {
+	  //console.log(JSON.stringify(data.songs)); 
+
+    for (var i= 0; i < data.songs.length; i++){
+      songString += "<h2 class='song-name'>" + data.songs[i].name + "</h2>";
+      songString += "<ul><li>" + data.songs[i].artist + "</li>";
+      songString += "<li>|</li>";
+      songString += "<li>" + data.songs[i].album + "</li>";
+      songString += "<li>" + "|" + "</li>";
+      songString += "<li>" + data.songs[i].year + "</li>";
+      songString += "<li>" + "|" + "</li>";
+      songString += "<li>" + data.songs[i].label + "</li></ul>";
+
+      
+      $("#theSongList").html(songString);
+           
+    } 
+	})   
+});
 
 
-for (var i = 0; i < songs.length; i++){
-	var songsAdd = songs[i];
-	var songToFront = prompt("Add to the front of the list or exit to quit.");
-	var songToEnd = prompt("Add song to the end of the list or exit to quit.");
-
-	if (songToFront.toLowerCase() === "exit") {
-		break;
-	} else {
-		songs.unshift(songToFront);
-	}
-	if (songToEnd.toLowerCase() === "exit") {
-		break;
-	} else {
-		songs.push(songToEnd);
-	}
-	}
-
-for (var j = 0; j < songs.length; j++) {
-	songs[j] = songs[j].replace(/[!@*(]/g, "");
-	songs[j] = songs[j].replace(">", "-");
-	songList.innerHTML += "<span class= 'song-list'>" + songs[j] + "</span>" + "<br>";
-}
